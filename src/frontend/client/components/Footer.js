eca49611a4fc47b3f617d6a4574d67d03ded04ba
pravinmfai <kolaapravin.mfai@gmail.com>
2025-02-14 12:58:18 +0530

    footer change

--- src/frontend/client/components/Footer.js
+++ src/frontend/client/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Footer.css';
 import logo from '../assest/icon.svg';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Footer = () => {
   return (
@@ -27,23 +28,23 @@ const Footer = () => {
           <div className="footer-contact">
             <p>5/191, Bettalada, Water falls, Kookalthorai Post, Kotagiri,</p>
             <p>Nilgiris - 643217</p>
-            <p>+91 XXXXX XXXXX</p>
-            <p>Blossoms@gmail.com</p>
+            <p>+91 74186 03504</p>
+            <p>blossomsbotique.mfai@gmail.com</p>
           </div>
         </div>
 
         {/* Social Media Icons */}
         <div className="footer-social">
-          <a href="#" className="icon-circle"><i className="fas fa-phone"></i></a>
-          <a href="#" className="icon-circle"><i className="fab fa-whatsapp"></i></a>
-          <a href="#" className="icon-circle"><i className="far fa-envelope"></i></a>
-          <a href="#" className="icon-circle"><i className="fab fa-instagram"></i></a>
+          <a href="tel:+917418603504" className="icon-circle"><i className="fas fa-phone"></i></a>
+          <a href="https://wa.me/917418603504?text=Hi!%20I%20found%20your%20boutique%20online%20and%20want%20to%20know%20more!" className="icon-circle"><i className="fab fa-whatsapp"></i></a>
+          <a href="mailto:blossomsbotique.mfai@gmail.com" className="icon-circle"><i className="far fa-envelope"></i></a>
+          <a href="https://www.instagram.com/blossomsboutique65" className="icon-circle"><i className="fab fa-instagram"></i></a>
         </div>
       </div>
 
       {/* Footer Bottom */}
       <div className="footer-bottom">
-        <p>© 2024 All Rights Reserved. Blossoms Boutique enhanced by MindFulAI</p>
+        <p>© 2024 All Rights Reserved. Blossoms Boutique enhanced by MindFulAI Pvt Ltd.</p>
       </div>
     </footer>
   );
